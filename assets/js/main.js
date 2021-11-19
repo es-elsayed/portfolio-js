047beb6cc15ea76317a535f3ac527fc460b1e021
islam-abdelkader <islamabdelkader22@gmail.com>
2021-11-20 01:15:11 +0200

    adding portfolio section

--- assets/js/main.js
+++ assets/js/main.js
@@ -90,7 +90,20 @@ modalCloses.forEach((modalClose) => {
 })
 
 /*==================== PORTFOLIO SWIPER  ====================*/
-
+var swiper = new Swiper(".portfolio__container", {
+    cssMode: true,
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    // mousewheel: true,
+    // keyboard: true,
+  });
 
 /*==================== TESTIMONIAL ====================*/
 
