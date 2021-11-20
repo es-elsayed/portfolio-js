4cfb5ec4eadc259e24118d7491e6b9686646a65a
islam-abdelkader <islamabdelkader22@gmail.com>
2021-11-20 12:44:44 +0200

    adding contact me section

--- assets/js/main.js
+++ assets/js/main.js
@@ -90,7 +90,7 @@ modalCloses.forEach((modalClose) => {
 })
 
 /*==================== PORTFOLIO SWIPER  ====================*/
-var swiper = new Swiper(".portfolio__container", {
+var swiperPortfolio = new Swiper(".portfolio__container", {
     cssMode: true,
     loop: true,
     navigation: {
@@ -101,12 +101,24 @@ var swiper = new Swiper(".portfolio__container", {
       el: ".swiper-pagination",
       clickable: true,
     },
-    // mousewheel: true,
-    // keyboard: true,
   });
 
 /*==================== TESTIMONIAL ====================*/
-
+var swiperTestimonial = new Swiper(".testimonial__container", {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 48,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true
+    },
+    breakpoints: {
+        568: {
+            slidesPerView: 2,
+        }
+    }
+  });
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 
